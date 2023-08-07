45d112db81368912c85c82ff3b03c0d6ab11f141
Christian Ege <ch@ege.io>
2023-08-07 17:09:11 +0200

    feat: add support for providing pointers as argumnets for get
    With this we can query different JSON pointers at a time. Or a single
    one in case we are more interested in a specific object.
    
    Signed-off-by: Christian Ege <ch@ege.io>

--- cmd/ovp8xx/cmd/get.go
+++ cmd/ovp8xx/cmd/get.go
@@ -11,8 +11,13 @@ import (
 )
 
 func getCommand(cmd *cobra.Command, args []string) error {
+
+	pointers, err := cmd.Flags().GetStringSlice("pointer")
+	if err != nil {
+		panic(err)
+	}
 	o3r := ovp8xx.NewClient()
-	query := []string{"/device"}
+	query := pointers
 	result, err := o3r.Get(query)
 	if err != nil {
 		return err
@@ -26,15 +31,36 @@ func getCommand(cmd *cobra.Command, args []string) error {
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Retrieve the JSON configuration from the device",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Long: `The OVP8xx get call accepts a list of JSON pointer like queries.
+Valid queries are for example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+- The empty string "" which queries the complete configuration
+- To query all ports including all sub elements the query "/ports" can be used.
+
+In contrast to the concept of a JSON pointer the OVP8xx does not response with the data
+the pointer is pointing to, it returns the full object hirachie with the encapsulating
+object paths.
+
+A query of the name of the "port6" (/ports/port6/info/name) not just returns the object of that port,
+it also keeps the hirachy intact:
+
+{
+	"ports":
+		"port6": {
+			"info": {
+				"name":"Front Left"
+			}
+		}
+}
+
+This allows one to use the response of a "get" command to directly feed it into a "set" command
+
+NOTE: This command only modifies temporary data, any changes will be lost after a reboot or power off.
+`,
 	RunE: getCommand,
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().StringSliceP("pointer", "p", []string{""}, "A JSON pointer to be queried")
 }
