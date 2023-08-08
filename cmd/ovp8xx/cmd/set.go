914c1f61e58e560b1745c7f58016c6abac8ddb17
Christian Ege <ch@ege.io>
2023-08-08 13:07:48 +0200

    feat: implement reading from stdin for the set command
    This will allow to use the ovp8xx cli inside of a bunch of shell
    commands "piped" together like so:
    
    echo '{"device":{"info":{"name":"Fred"}}}'| ovp8xx set
    
    Signed-off-by: Christian Ege <ch@ege.io>

--- cmd/ovp8xx/cmd/set.go
+++ cmd/ovp8xx/cmd/set.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 
 	"github.com/graugans/go-ovp8xx/pkg/ovp8xx"
 	"github.com/spf13/cobra"
@@ -16,19 +17,34 @@ var setCmd = &cobra.Command{
 	Use:   "set",
 	Short: "Change the configuration of the device",
 	Long: `This will send a JSON configuration to the device
+
+If no argument is provided the "set" command reads input data from stdin.
+
+The input data is validated to contain a proper JSON string.
+There is only one JSON string allowed as an input
 	
 NOTE: Sending smaller bites of JSON will reduce the chewing costs on the device.
 Reduce your JSON to the max, only send the bare minimum required to achieve your goals
 Not just copy paste the output of a get all ("") with a small change into a set.
 Better extract the object you want to change and only send this`,
-	Args: cobra.ExactArgs(1),
+	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return fmt.Errorf("invalid number of arguments provided to 'set'")
-		}
 
-		if !json.Valid([]byte(args[0])) {
-			return fmt.Errorf("the import argument is not valid JSON")
+		var data []byte
+		var err error
+
+		if len(args) == 1 {
+			// A commandline arg was given let's use this
+			data = []byte(args[0])
+		} else {
+			// No input Argument was provided try to ready from stdin
+			if data, err = io.ReadAll(cmd.InOrStdin()); err != nil {
+				return err
+			}
+
+		}
+		if !json.Valid(data) {
+			return fmt.Errorf("the input argument is not valid JSON")
 		}
 
 		host, err := rootCmd.PersistentFlags().GetString("ip")
@@ -42,7 +58,7 @@ Better extract the object you want to change and only send this`,
 
 		return o3r.Set(
 			*ovp8xx.NewConfig(
-				ovp8xx.WitJSONString(args[0]),
+				ovp8xx.WitJSONString(string(data)),
 			),
 		)
 	},
