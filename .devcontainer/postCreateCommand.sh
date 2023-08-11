380620a7929703e0977b84ec8897d2c21df275e4
Christian Ege <ch@ege.io>
2023-08-11 07:01:20 +0200

    build: install the golangci-lint package
    Since we do run this in the CI we also want to have it in the local dev
    container
    
    Signed-off-by: Christian Ege <ch@ege.io>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go install github.com/goreleaser/goreleaser@latest
-
+# Go CI Lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.0
